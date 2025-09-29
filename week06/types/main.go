75cc0bbfacc833519520aa4e21591bb785deffa2
quequuen <rkdusl2020@gmail.com>
2025-09-29 10:37:31 +0900

    v0.1 types/main.go - 자료형 수업을 위한 types 폴더 생성 - main.go 생성

---
+++ week06/types/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main(){
+	fmt.Println(math.Floor(3.14));
+	fmt.Println(strings.Title("head first go"));
+	fmt.Println(math.Round(2.31));
+}
