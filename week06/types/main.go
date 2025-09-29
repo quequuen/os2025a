1a6cda0fffbe89e99cdbedaaffae0136bad6e305
quequuen <rkdusl2020@gmail.com>
2025-09-29 12:13:18 +0900

    v0.4 Add: Naming rules

--- week06/types/main.go
+++ week06/types/main.go
@@ -11,6 +11,7 @@ func main(){
 	{
 		name:="Kim Inha"
 		// ':='는 선언할 때 쓰는 거기 때문에 재할당 용도로는 쓰지 못함.
+		// 단축 변수 선언문(short variable declartion)
 		// 이것도 할당임
 
 		// var name string
@@ -37,6 +38,23 @@ func main(){
 		fmt.Println(str, reflect.TypeOf(str))
 		fmt.Println(i32, reflect.TypeOf(i32))
 		fmt.Println(b, reflect.TypeOf(b))
+	}
+	{
+		//Naming rules
+		
+		// 1. 숫자로 시작할 수 없다. (영어로 시작해야만 함.)
+		// var 64f float64
+
+		// 2. 추가적인 문자나 숫자의 제약이 없다.
+		
+		// 3. 대소문자 구분을 분명히 한다.
+		// fmt.println()
+		// +) 앞 글자를 소문자로 하게 되면 외부에서 접근이 불가능하다. 강제 static화..?
+
+		// 4. 관례상 변수명이나 함수명은 카멜케이스로 해라
+
+		// 5. 최대한 생략해라
+		//  index -> i
 	}
 	
 }
