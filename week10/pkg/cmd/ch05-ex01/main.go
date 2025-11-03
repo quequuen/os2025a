6ad720332a3a658dce010dd85550467559da47c0
quequuen <rkdusl2020@gmail.com>
2025-11-03 11:52:33 +0900

    Add: week10 array 학습

---
+++ week10/pkg/cmd/ch05-ex01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var arrayBool [3]bool
+	var arrayInt [3]int
+
+	fmt.Println(arrayBool[1])	//zero value
+	arrayInt[1]++
+	arrayInt[1]++
+	fmt.Println(arrayInt[1])	//zero value + 2
+}
