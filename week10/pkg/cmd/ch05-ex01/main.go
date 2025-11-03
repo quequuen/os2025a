78bb10d8f3e7ff6bc7577e246e89a5a043fb3f14
quequuen <rkdusl2020@gmail.com>
2025-11-03 12:10:01 +0900

    Add: week10  반복문과 반복문 내 range 응용 학습

--- week10/pkg/cmd/ch05-ex01/main.go
+++ week10/pkg/cmd/ch05-ex01/main.go
@@ -15,7 +15,33 @@ func main(){
 	//array 리터럴(선언과 동시에 할당)
 	var arrayBool2 [3]bool = [3]bool{true, false, false}
 	fmt.Println(arrayBool2)
+	fmt.Printf("%#v\n", arrayBool2)
 
 	arrayInt2 := [3]int{1,2,3}
 	fmt.Println(arrayInt2)
+	fmt.Printf("%#v\n", arrayInt2)
+
+	//반복문
+	arrayBool3 := [3]bool{true, false, true} 
+	arrayInt3 := [3]int{-9, 11, 7}
+
+	for i := 0; i < len(arrayInt3); i++ {
+		fmt.Println(i, arrayInt3[i])
+		fmt.Println(i, arrayBool3[i])
+	}
+
+	//반복문 range 쓰는 예제 
+	// numbers := [3]int{-9, 11, 7}
+
+	// for i := 0; i < len(arrayInt3); i++ {
+	// 	fmt.Println(i, numbers[i])
+	// }
+
+	numbers := [3]int{-9, 11, 7}
+
+	for i, number := range numbers {
+		fmt.Println(i, number)
+	}
+
+
 }
