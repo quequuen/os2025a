f2a4a138fab886102c42c55356063b390de26473
quequuen <rkdusl2020@gmail.com>
2025-11-03 11:58:06 +0900

    Add: week10 array literal 학습

--- week10/pkg/cmd/ch05-ex01/main.go
+++ week10/pkg/cmd/ch05-ex01/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main(){
+	//배열(선언 후 할당)
 	var arrayBool [3]bool
 	var arrayInt [3]int
 
@@ -10,4 +11,11 @@ func main(){
 	arrayInt[1]++
 	arrayInt[1]++
 	fmt.Println(arrayInt[1])	//zero value + 2
+
+	//array 리터럴(선언과 동시에 할당)
+	var arrayBool2 [3]bool = [3]bool{true, false, false}
+	fmt.Println(arrayBool2)
+
+	arrayInt2 := [3]int{1,2,3}
+	fmt.Println(arrayInt2)
 }
