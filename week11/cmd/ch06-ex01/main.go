b687ba65d76248062c69c638e59079be8282264f
quequuen <rkdusl2020@gmail.com>
2025-11-10 11:28:09 +0900

    Add: week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -6,11 +6,15 @@ func main(){
 	// 슬라이스 리터럴 선언 및 할당
 	// var subjects []string 
 	// subjects = make([]string,3)
-	subjects := []string{"Go", "", "Python"}
-	
+	subjects := []string{"Go", "Javascript", "Python", "Linux"}
+	subjectsSlice := subjects[1:3]
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
+	}
+	fmt.Println("----슬라이스 일부만 출력----")
+	for i:=0; i<len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
 	}
 	
 }
