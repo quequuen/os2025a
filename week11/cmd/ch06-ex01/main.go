3b25ecae50626456b8522d43a67c5271a54bddd8
quequuen <rkdusl2020@gmail.com>
2025-11-10 11:37:22 +0900

    Add: week11 slice v0.4

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -8,7 +8,10 @@ func main(){
 	// subjects = make([]string,3)
 	subjects := []string{"Go", "Javascript", "Python", "Linux"}
 	subjectsSlice := subjects[1:3]
+	// 슬라이스는 범위를 넘을 수 없다!
+	// subjectsSlice := subjects[1:4] //에러 발생
 
+	fmt.Println("----슬라이스 전체 출력----")
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
@@ -16,5 +19,15 @@ func main(){
 	for i:=0; i<len(subjectsSlice); i++ {
 		fmt.Println(subjectsSlice[i])
 	}
+
+	// 배열 리터럴을 슬라이싱 하면 슬라이스 리터럴이 됨.(배열을 이용해 슬라이스 생성 가능)
+	// 해당 동작은 깊은 복사가 아닌 얕은 복사로 값이 바뀌면 그 값을 공유함.(포인터 개념)
+	array3:= [5]string{"C", "C++", "Java", "HTML", "CSS"}
+	arraySlice := array3[2:4]
+	array3[2] = "ChangedJava"
+	arraySlice[0] = "ChangedAgainJAVA"
+	
+	fmt.Println(array3)
+	fmt.Println(arraySlice)
 	
 }
