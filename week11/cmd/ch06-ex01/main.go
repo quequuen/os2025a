ebeb60fbb1a9ff0be516b46c2ec45b9f7cce2604
quequuen <rkdusl2020@gmail.com>
2025-11-10 11:23:28 +0900

    Add: week11 slice v0.2

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main(){
 	// 슬라이스 리터럴 선언 및 할당
-	var subjects []string 
-	subjects = make([]string,3)
-	subjects[0] ="Go"
-	subjects[2] = "Python"
+	// var subjects []string 
+	// subjects = make([]string,3)
+	subjects := []string{"Go", "", "Python"}
+	
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
