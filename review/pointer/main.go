11bff3596d47614e12cc1c9429364a5fa45d5bf7
quequuen <rkdusl2020@gmail.com>
2025-11-27 21:23:20 +0900

    Add: review/pointer 포인터 복습 추가

---
+++ review/pointer/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main(){
+	// 포인터 기본 사용법
+	{
+		amount := 6
+		fmt.Println(amount)
+		fmt.Println(&amount)
+	
+		var myInt int
+		fmt.Println(&myInt)
+		var myFloat float64
+		fmt.Println(&myFloat)
+		var myBool bool
+		fmt.Println(&myBool)
+	}
+
+	//reflect로 타입 확인
+	{
+		var myInt int
+		fmt.Println(reflect.TypeOf(&myInt))
+		var myFloat float64
+		fmt.Println(reflect.TypeOf(&myFloat))
+		var myBool bool
+		fmt.Println(reflect.TypeOf(&myBool))
+		// *int
+		// *float64
+		// *bool
+		// 각각의 포인터를 가져와 포인터 타입을 출력
+	}
+
+	// 포인터 변수 선언
+	// 한 가지 타입에 대한 포인터 값만 가질 수 있음
+
+	var myInt int
+	// var myIntPointer *int = &myInt
+	myIntPointer := &myInt
+	fmt.Println(myIntPointer)
+
+	var myFloat float64
+	myFloatPointer := &myFloat
+	// 포인터 변수도 마찬가지로 단축 변수 선언 사용 가능
+	fmt.Println(*myFloatPointer)
+	// & -> 주소를 줄 때 사용
+	// * -> 해당 주소의 값을 가져올 때 사용
+
+
+}
