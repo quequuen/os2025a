433a41dd79e9ea2a0146ff0c1ca72df8e0864248
quequuen <rkdusl2020@gmail.com>
2025-12-01 12:03:21 +0900

    Add:week14 goroutine v0.2

--- week14/cmd/ch13-ex01/main.go
+++ week14/cmd/ch13-ex01/main.go
@@ -8,11 +8,15 @@ import (
 func say(msg string) {
 	for i := 0; i < 3; i++ {
 		fmt.Println(msg, ":", i)
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(2000 * time.Millisecond)
 	}
 }
 
 func main() {
-	go say("고루틴") // 새 고루틴에서 실행
-	say("메인")     // 메인 고루틴에서 실행
+	start := time.Now()
+	say("고루틴") // 새 고루틴에서 실행
+	say("메인")  // 메인 고루틴에서 실행
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("전체 실행 시간 : ", time.Since(start))
 }
