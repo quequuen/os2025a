1ff9fe1ce8b206aeb52109f6c1a9ca5dcd5b83ef
quequuen <rkdusl2020@gmail.com>
2025-12-03 02:02:03 +0900

    Add: review 내 배열, 배열 리터럴 추가

---
+++ review/array/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// 배열
+// 배열의 번호: 인덱스(index)
+// 배열은 선언한 크기가 고정되어 변하지 않는다.
+func main(){
+	// 배열 리터럴
+	// 선언과 동시에 초기값을 다른 값으로 할당
+	var notesliteral [7]string = [7]string{"do","re","mi"}
+	fmt.Println(notesliteral)
+
+
+	// 기본 문자열 배열
+	// 선언과 동시에 초기값들로 채워짐. -> 추후에 하나씩 채움
+	var notes [7]string
+	notes[0] = "do"
+
+	fmt.Println(notes) //[do      ]
+}
