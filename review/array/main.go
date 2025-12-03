b0b215a028071a68da2c76fe72203f2582a2e1a6
quequuen <rkdusl2020@gmail.com>
2025-12-03 14:15:49 +0900

    Add: review 내 배열 리터럴 range 사용 조회

--- review/array/main.go
+++ review/array/main.go
@@ -6,10 +6,18 @@ import "fmt"
 // 배열의 번호: 인덱스(index)
 // 배열은 선언한 크기가 고정되어 변하지 않는다.
 func main(){
+	
+
+
+	// 배열의 범위를 벗어난 인덱스를 조회하려고 하면 패닉 발생
+
 	// 배열 리터럴
 	// 선언과 동시에 초기값을 다른 값으로 할당
 	var notesliteral [7]string = [7]string{"do","re","mi"}
 	fmt.Println(notesliteral)
+	for _, note := range notesliteral{
+		fmt.Println(note)
+	}
 
 
 	// 기본 문자열 배열
