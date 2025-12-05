b2eb91e0fc90e3ada75d84408b3b9d08e92b1b0d
quequuen <rkdusl2020@gmail.com>
2025-12-05 23:43:12 +0900

    Add: review/slice 내 슬라이스 확장과 내부 배열 변경 복습 내용 추가

--- review/slice/main.go
+++ review/slice/main.go
@@ -1,17 +1,37 @@
 package main
 
+import "fmt"
+
 
 
 func main(){
 
+	//내부 배열 변경
+	array1 := [5]string{"A","B","C","D","E"}
+	slice1 := array1[1:4] // 배열의 일부를 참조하는 슬라이스 생성
+	slice1[0] = "X"       // 슬라이스를 통해 내부 배열 변경
+	fmt.Print(array1)         // [A X C D E] 출력
 
+	//슬라이스 확장
+	slice2 := array1[2:4] // 배열의 일부를 참조하는 슬라이스 생성
+	slice2 = append(slice2, "Y") // 슬라이스 확장
+	fmt.Print(slice2)                // [C D Y] 출력
+	fmt.Print(array1)                // [A X C D Y] 출력, 내부 배열도 변경됨
+
+	//슬라이스 중복 내부 배열 변경
+	slice3 := array1[1:4] // 배열의 일부를 참조하는 슬라이스 생성
+	slice4 := array1[2:5] // 배열의 일부를 참조하는 슬라이스 생성
+	slice3[1] = "Z"       // 슬라이스3을 통해 내부 배열 변경
+	fmt.Print(array1)         // [A X Z D Y] 출력
+	fmt.Print(slice3)         // [X Z D] 출력
+	fmt.Print(slice4)         // [Z D Y] 출력, 슬라이스4도 변경됨
 
 	// 슬라이스 리터럴
 	notesliteral := []string{"one","two","three","four","five","six","seven"}
 	for i := range notesliteral{
-		print(notesliteral[i])
+		fmt.Print(notesliteral[i])
 	}
-	print("\n")
+	fmt.Print("\n")
 
 
 
